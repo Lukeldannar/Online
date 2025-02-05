7943b6a6e8ed0167405dae7c8b2381b7ba9e1655
Lukeldannar <159046823+Lukeldannar@users.noreply.github.com>
2025-02-05 18:38:10 +0000

    Updated

--- Week1/Due/024-026/026.py
+++ Week1/Due/024-026/026.py
@@ -5,7 +5,6 @@ if word[0] == "a" or word[0] == "e" or word[0] == "i" or word[0] == "o" or word[
     print (word)
 else:
     num_cha = len (word) - 1
-    print (num_cha)
     list = []
     tot = -1
     for i in word:
@@ -16,7 +15,7 @@ else:
             list.append (word[0])
         else:
             list.append (word[tot+1])
-    fin = None
-    for i in list:
-        None
-    print (list)
+    fin = ""
+    fin = fin.join (list)
+    fin = fin + "ay"
+    print (fin)
